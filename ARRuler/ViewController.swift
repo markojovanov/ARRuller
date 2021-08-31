5144d8329c0194afb9fd610ada289cddd2ca0414
Marko Jovanov <markojovanov999@gmail.com>
2021-08-31 10:49:20 +0200

    Refactor

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -10,34 +10,24 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-    
     @IBOutlet var sceneView: ARSCNView!
     var dotNodes = [SCNNode]()
     var textNode = SCNNode()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-        
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if dotNodes.count >= 2 {
             for dot in dotNodes {
@@ -51,14 +41,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             if let hitResult = hitTestResult.first {
                 addDot(at: hitResult)
             }
-            
         }
     }
     func addDot(at hitResult: ARHitTestResult) {
         let dotGeometry = SCNSphere(radius: 0.005)
-        let dotMaterial = SCNMaterial()
-        dotMaterial.diffuse.contents = UIColor.red
-        dotGeometry.materials = [dotMaterial]
+        dotGeometry.firstMaterial?.diffuse.contents = UIColor.red
         let dotNode = SCNNode(geometry: dotGeometry)
         dotNode.position = SCNVector3(
             hitResult.worldTransform.columns.3.x,
@@ -74,10 +61,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func calculate() {
         let start = dotNodes[0]
         let end = dotNodes[1]
-        
         let distance = sqrt(
             pow(end.position.x - start.position.x, 2) +
-            pow(end.position.y  - start.position.y, 2) +
+                pow(end.position.y  - start.position.y, 2) +
                 pow(end.position.z - start.position.z, 2)
         )
         updateText(text: "\(abs(distance))",at: end.position)
